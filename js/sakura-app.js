fe2b66e0ca06362d147cff455db997827dfa87b4
starlovei <star_love_i@outlook.com>
2022-05-19 23:17:55 +0800

    Site updated: 2022-05-19 23:17:54

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -170,20 +170,20 @@ function imgError(ele, type) {
   switch (type) {
     case 1:
       if (ele.src.includes('https://cn.gravatar.com/avatar')) {
-        ele.src = ele.src.replace('https://cn.gravatar.com/avatar/', 'https://cdn.jsdelivr.net/gh/starlovei/cdn/img/other/gravatar.jpg')
+        ele.src = ele.src.replace('https://cn.gravatar.com/avatar/', 'https://fastly.jsdelivr.net/gh/starlovei/cdn/img/other/gravatar.jpg')
       } else {
-        ele.src = 'https://cdn.jsdelivr.net/gh/starlovei/cdn/img/other/default_avatar.jpg'
+        ele.src = 'https://fastly.jsdelivr.net/gh/starlovei/cdn/img/other/default_avatar.jpg'
       }
       break
     case 2:
-      ele.src = 'https://cdn.jsdelivr.net/gh/starlovei/cdn/img/other/default_gavatar.jpg'
+      ele.src = 'https://fastly.jsdelivr.net/gh/starlovei/cdn/img/other/default_gavatar.jpg'
       break
     case 3:
 
-      ele.src = 'https://cdn.jsdelivr.net/gh/starlovei/cdn/img/other/image-404.png'
+      ele.src = 'https://fastly.jsdelivr.net/gh/starlovei/cdn/img/other/image-404.png'
       break
     default:
-      ele.src = 'https://cdn.jsdelivr.net/gh/starlovei/cdn/img/other/image-404.png'
+      ele.src = 'https://fastly.jsdelivr.net/gh/starlovei/cdn/img/other/image-404.png'
   }
 }
 mashiro_global.post_list_show_animation = new function () {
@@ -321,7 +321,7 @@ function attach_image() {
             $('.insert-image-tips').html('<i class="fa fa-picture-o" aria-hidden="true"></i>')
           }, 1000)
           var get_the_url = res.data.url.replace('https://i.loli.net/', 'https://static.shino.cc/user-upload/')
-          $('#upload-img-show').append('<img class="lazyload upload-image-preview" src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.0.2/img/svg/loader/trans.ajax-spinner-preloader.svg" data-src="' + get_the_url + '" onclick="window.open(\'' + get_the_url + '\')" onerror="imgError(this)" />')
+          $('#upload-img-show').append('<img class="lazyload upload-image-preview" src="https://fastly.jsdelivr.net/gh/moezx/cdn@3.0.2/img/svg/loader/trans.ajax-spinner-preloader.svg" data-src="' + get_the_url + '" onclick="window.open(\'' + get_the_url + '\')" onerror="imgError(this)" />')
           lazyload()
           addComment.createButterbar('图片上传成功~<br>Uploaded successfully~')
           grin(res.data.url.replace('https://i.loli.net/', '{UPLOAD}'), type = 'Img')
@@ -432,7 +432,7 @@ function checkBgImgCookie() {
     $('#banner_wave_2').addClass('banner_wave_hide_fit_skin')
   }
   if (bgurl != '') {
-    if (bgurl == 'https://cdn.jsdelivr.net/gh/starlovei/cdn/img/themebg/sakura.png' || bgurl == 'https://cdn.jsdelivr.net/gh/starlovei/cdn/img/themebg/plaid.jpg' || bgurl == 'https://cdn.jsdelivr.net/gh/starlovei/cdn/img/themebg/star.png' || bgurl == 'https://cdn.jsdelivr.net/gh/starlovei/cdn/img/themebg/point.png' || bgurl == 'https://cdn.jsdelivr.net/gh/starlovei/cdn/img/themebg/little-monster.png') {
+    if (bgurl == 'https://fastly.jsdelivr.net/gh/starlovei/cdn/img/themebg/sakura.png' || bgurl == 'https://fastly.jsdelivr.net/gh/starlovei/cdn/img/themebg/plaid.jpg' || bgurl == 'https://fastly.jsdelivr.net/gh/starlovei/cdn/img/themebg/star.png' || bgurl == 'https://fastly.jsdelivr.net/gh/starlovei/cdn/img/themebg/point.png' || bgurl == 'https://fastly.jsdelivr.net/gh/starlovei/cdn/img/themebg/little-monster.png') {
       mashiro_global.variables.skinSecter = true
       mashiro_global.variables.isNight = false
       $('#night-mode-cover').css('visibility', 'hidden')
@@ -499,11 +499,11 @@ $(document).ready(function () {
       setCookie('bgImgSetting', url, 30)
     })
   }
-  changeBG('#sakura-bg', 'https://cdn.jsdelivr.net/gh/starlovei/cdn/img/themebg/sakura.png')
-  changeBG('#gribs-bg', 'https://cdn.jsdelivr.net/gh/starlovei/cdn/img/themebg/plaid.jpg')
-  changeBG('#pixiv-bg', 'https://cdn.jsdelivr.net/gh/starlovei/cdn/img/themebg/star.png')
-  changeBG('#KAdots-bg', 'https://cdn.jsdelivr.net/gh/starlovei/cdn/img/themebg/point.png')
-  changeBG('#totem-bg', 'https://cdn.jsdelivr.net/gh/starlovei/cdn/img/themebg/little-monster.png')
+  changeBG('#sakura-bg', 'https://fastly.jsdelivr.net/gh/starlovei/cdn/img/themebg/sakura.png')
+  changeBG('#gribs-bg', 'https://fastly.jsdelivr.net/gh/starlovei/cdn/img/themebg/plaid.jpg')
+  changeBG('#pixiv-bg', 'https://fastly.jsdelivr.net/gh/starlovei/cdn/img/themebg/star.png')
+  changeBG('#KAdots-bg', 'https://fastly.jsdelivr.net/gh/starlovei/cdn/img/themebg/point.png')
+  changeBG('#totem-bg', 'https://fastly.jsdelivr.net/gh/starlovei/cdn/img/themebg/little-monster.png')
   changeBGnoTrans('#bing-bg', 'https://api.ixiaowai.cn/gqapi/gqapi.php')
   $('.skin-menu #white-bg').click(function () {
     mashiro_global.variables.skinSecter = false
@@ -521,7 +521,7 @@ $(document).ready(function () {
   $('.skin-menu #dark-bg').click(function () {
     mashiro_global.variables.skinSecter = true
     mashiro_global.variables.isNight = true
-    $('body').css('background-image', 'url(https://cdn.jsdelivr.net/gh/starlovei/cdn/img/other/starry_sky.jpg)')
+    $('body').css('background-image', 'url(https://fastly.jsdelivr.net/gh/starlovei/cdn/img/other/starry_sky.jpg)')
     $('.blank').css('background-color', 'rgba(255,255,255,.8)')
     $('#night-mode-cover').css('visibility', 'visible')
     // $('.pattern-center').removeClass('pattern-center').addClass('pattern-center-sakura')
@@ -1611,9 +1611,9 @@ if ((isWebkit || isOpera || isIe) && document.getElementById && window.addEventL
 }
 // loadCSS(mashiro_option.jsdelivr_css_src);
 // loadCSS("https://at.alicdn.com/t/font_679578_dishi1yoavm.css");
-// loadCSS("https://cdn.jsdelivr.net/gh/moezx/cdn@3.5.4/fonts/Moe-Mashiro/stylesheet.css");
+// loadCSS("https://fastly.jsdelivr.net/gh/moezx/cdn@3.5.4/fonts/Moe-Mashiro/stylesheet.css");
 // loadCSS("https://fonts.googleapis.com/css?family=Noto+SerifMerriweather|Merriweather+Sans|Source+Code+Pro|Ubuntu:400,700");
-// loadCSS("https://cdn.jsdelivr.net/gh/moezx/cdn@3.3.9/css/sharejs.css");;
+// loadCSS("https://fastly.jsdelivr.net/gh/moezx/cdn@3.3.9/css/sharejs.css");;
 
 function render(template, context) {
   var tokenReg = /(\\)?\{([^\{\}\\]+)(\\)?\}/g
